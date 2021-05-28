2a5ef00ff06d86ed4de9b49dced607dd0d868b58
MarvelTiter <outlinmt@outlook.com>
2021-05-28 17:12:48 +0800

    代码备份

---
+++ Shared.DataTableUtils.Core/DataTableHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+
+namespace Shared.DataTableUtils.Core {
+    public static class DataTableHelper {
+        public static bool HasRows(this DataTable dt) {
+            return dt != null && dt.Rows.Count > 0;
+        }
+
+        public static IEnumerable<T> ToEnumerable<T>(this DataTable dt) {
+            foreach (DataRow item in dt.Rows) {
+                yield return item.Parse<T>();
+            }
+        }
+
+        public static T Parse<T>(this DataRow row) {
+            return default;
+        }
+    }
+}
