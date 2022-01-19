855cadda892f0b2faf64e010e029692114be72a8
MarvelTiter <outlinmt@outlook.com>
2022-01-19 14:54:13 +0800

    备份

--- LoggerConsoleTest/Program.cs
+++ LoggerConsoleTest/Program.cs
@@ -1,17 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using MT.KitTools.LogTool;
-
-Logger.Enable(LogType.Console | LogType.File);
+Logger.EnableAllDefault();
 for (int i = 0; i < 10; i++)
 {
-    Logger.Warn($"测试{i}");
-    Task.Delay(1000).Wait();
-}
-Logger.Disable(LogType.File);
-for (int i = 0; i < 10; i++)
-{
-    Logger.Warn($"测试{i}");
+    Logger.Info($"测试{i}");
     Task.Delay(1000).Wait();
 }
 Console.Read();
